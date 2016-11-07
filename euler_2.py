4435a80c0f1ed03b4e4c55925cff2b8ce4fc7902
kevin ly <kevinly630@gmail.com>
2016-11-06 23:41:57 -0500

    fixed euler_2.py

--- euler_2.py
+++ euler_2.py
@@ -2,14 +2,14 @@ limit = 4000000
 var1 = 0
 var2 = 1
 sum = 0
+seq = 0
 
-for i in range (1, limit):
+while seq < limit:
     seq = var1 + var2
     if seq % 2 == 0:
         sum = sum + seq
-    nextSeq  = var1 + var2
     var1 = var2
-    var2 = nextSeq
+    var2 = seq
 
 print (sum)
     
