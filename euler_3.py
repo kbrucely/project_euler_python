d41068b6aa1d65e602afaf0f927b1ad30d1c0656
kevin ly <kevinly630@gmail.com>
2016-12-05 20:40:36 -0500

    fixed euler3

--- euler_3.py
+++ euler_3.py
@@ -7,13 +7,14 @@ def isPrime ( x ):
 
     return True
 
-test = 13195
-#test = 600851475143
+#test = 13195
+test = 600851475143
 largest = 0
 
-for x in xrange (2, test):
-    if isPrime(x):
-        if test % x == 0:
+#it is very important to check first if a number is a factor and then if it's prime and not the other way around.  the first way takes weeks.  
+for x in xrange (2, int(math.floor(math.sqrt(test)))):
+    if test % x == 0:
+        if isPrime(x):
             largest = x
 
 print (largest)
